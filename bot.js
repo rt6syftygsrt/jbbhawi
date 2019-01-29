de62893998e6bf7a172200b6701116a5a81ba231
saaid35 <46090530+rt6syftygsrt@users.noreply.github.com>
2019-01-29 18:46:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,26 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
-client.on('ready', () => {
-console.log(Logged in as ${client.user.tag}!);
-client.user.setGame(Nothing,"http://twitch.tv/S-F%22)
-console.log('')
-console.log('')
-console.log('╔[════════════════════════════════════
-console.log([Start] ${new Date()});
-console.log('╚[════════════════════════════════════
-console.log('')
-console.log('╔[════════════════════════════════════]
-console.log(Logged in as * [ " ${client.user.username} " ]);
-console.log('')
-console.log('Informations :')
-console.log('')
-console.log(servers! [ " ${client.guilds.size} " ]);
-console.log(Users! [ " ${client.users.size} " ]);
-console.log(channels! [ " ${client.channels.size} " ]);
-console.log('╚[════════════════════════════════════]
-console.log('')
-console.log('╔[════════════]╗')
-console.log(' Bot Is Online')
-console.log('╚[════════════]╝')
-console.log('')
-console.log('')
+
+
+
+
+client.login('NTM5ODMxNTQwMzY5MzI2MTEw.DzIE2A.XX0qFh2_JFdzNKIVU5A2RAL0oGs'); 
+
+
+
+client.on('ready',  () => {
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+  console.log('by safa7');
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log(`Logged in as  * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log('is online')
+client.user.setStatus("dnd");
 });
-client.login(process.env.BOT_TOKEN);
+
+// playing
+client.on('ready', () => {                           
+client.user.setGame(``,'https://www.twitch.tv/tarikrs');                                                                                                                                                                                                                                                                                                                                                                                                                             client.user.setGame(`-help | On 20 Servers`,'https://www.twitch.tv/v5bz');
+});          
+
+
